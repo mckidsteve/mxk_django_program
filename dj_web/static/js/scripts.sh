390ff101b970f6efa29367c9c451c149663319f2
dream li <734942701@qq.com>
2023-03-05 16:12:32 +0800

    git pr for web.html

--- dj_web/static/js/scripts.sh
+++ dj_web/static/js/scripts.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
 
+JS_PATH=~/home/dj_mck/dj_work/dj_web/static/js/
+JS_PATH_DIST=${JS_PATH}/dist/
+JS_PATH_SRC=${JS_PATH}/src/
+
+find ${JS_PATH_SRC} -type f -name "*.js" | sort | xargs cat > ${JS_PATH_DIST}game.js
